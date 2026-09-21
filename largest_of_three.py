649c735422068208e8be3c24768cbe013db28918
Sariga Santhosh <sarigasanthoshofficial@gmail.com>
2026-09-21 12:30:18 +0530

    Removing python files from web folder

--- largest_of_three.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter 1st number :"))
-b = int(input("Enter 2nd number :"))
-c = int(input("Enter 3rd number :"))
-largest = a
-if b > largest:
-  largest = b
-if c > largest:
-  largest = c
-print(largest)
--- loop.py
+++
@@ -1,7 +0,0 @@
-list = []
-
-for i in range(3):
-    a = int(input("Enter number: "))
-    list.append(a)
-
-print(max(list))
